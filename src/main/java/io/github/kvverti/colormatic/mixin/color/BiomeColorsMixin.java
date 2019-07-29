5662b29394594002379c9d88daa9beac4a11b33b
Thalia Nero <chrisvn00@gmail.com>
2019-07-28 21:00:56 -0400

    Fix water not respecting custom biome colormaps

--- src/main/java/io/github/kvverti/colormatic/mixin/color/BiomeColorsMixin.java
+++ src/main/java/io/github/kvverti/colormatic/mixin/color/BiomeColorsMixin.java
@@ -18,9 +18,13 @@
 package io.github.kvverti.colormatic.mixin.color;
 
 import io.github.kvverti.colormatic.Colormatic;
+import io.github.kvverti.colormatic.colormap.BiomeColormaps;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.client.color.world.BiomeColors;
+import net.minecraft.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ExtendedBlockView;
 import net.minecraft.world.biome.Biome;
 
 import org.spongepowered.asm.mixin.Dynamic;
@@ -40,6 +44,18 @@ public abstract class BiomeColorsMixin {
     private static void onWaterColor(Biome biome, BlockPos pos, CallbackInfoReturnable<Integer> info) {
         if(Colormatic.WATER_COLORS.hasCustomColormap()) {
             info.setReturnValue(Colormatic.WATER_COLORS.getColormap().getColor(biome, pos));
+        }
+    }
+
+    /**
+     * The FluidRenderer calls BiomeColors#getWaterColor directly, instead of
+     * going through BlockColors. So, we must check for custom water color here.
+     */
+    @Inject(method = "getWaterColor", at = @At("HEAD"), cancellable = true)
+    private static void onWaterColorPre(ExtendedBlockView world, BlockPos pos, CallbackInfoReturnable<Integer> info) {
+        BlockState state = world.getBlockState(pos);
+        if(state.getFluidState().matches(FluidTags.WATER) && BiomeColormaps.isCustomColored(state)) {
+            info.setReturnValue(BiomeColormaps.getBiomeColor(state, world, pos));
         }
     }
 
