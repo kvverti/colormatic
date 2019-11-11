d3021d5e06640d1ad819c4f503d80f63563ec2e8
Thalia Nero <chrisvn00@gmail.com>
2019-11-10 20:20:41 -0500

    Add back biome blending

--- src/main/java/io/github/kvverti/colormatic/mixin/world/ClientWorldMixin.java
+++ src/main/java/io/github/kvverti/colormatic/mixin/world/ClientWorldMixin.java
@@ -17,6 +17,7 @@
  */
 package io.github.kvverti.colormatic.mixin.world;
 
+import net.minecraft.client.MinecraftClient;
 import io.github.kvverti.colormatic.colormap.ColormaticResolver;
 import io.github.kvverti.colormatic.colormap.BiomeColormaps;
 import io.github.kvverti.colormatic.colormap.ColormaticBlockRenderView;
@@ -73,8 +74,21 @@ public abstract class ClientWorldMixin extends World implements ColormaticBlockR
 
     @Override
     public int colormatic_getColor(BlockPos pos, ColormaticResolver resolver) {
-        // todo: implement biome blend radius
-        Biome biome = this.getBiomeAccess().getBiome(pos);
-        return resolver.getColor(biome, pos);
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        int radius = MinecraftClient.getInstance().options.biomeBlendRadius;
+        Iterable<BlockPos> coll = BlockPos.iterate(
+            pos.getX() - radius, pos.getY(), pos.getZ() - radius,
+            pos.getX() + radius, pos.getY(), pos.getZ() + radius);
+        for(BlockPos curpos : coll) {
+            Biome biome = this.getBiomeAccess().getBiome(curpos);
+            int color = resolver.getColor(biome, curpos);
+            r += (color & 0xff0000) >> 16;
+            g += (color & 0x00ff00) >> 8;
+            b += (color & 0x0000ff);
+        }
+        int posCount = (radius * 2 + 1) * (radius * 2 + 1);
+        return ((r / posCount & 255) << 16) | ((g / posCount & 255) << 8) | (b / posCount & 255);
     }
 }
