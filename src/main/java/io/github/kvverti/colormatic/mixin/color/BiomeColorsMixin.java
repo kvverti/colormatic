6cbbae1acff07ccebc54ba9d61c648f6481cb457
Thalia Nero <chrisvn00@gmail.com>
2019-11-10 20:12:21 -0500

    Water colors

--- src/main/java/io/github/kvverti/colormatic/mixin/color/BiomeColorsMixin.java
+++ src/main/java/io/github/kvverti/colormatic/mixin/color/BiomeColorsMixin.java
@@ -17,6 +17,8 @@
  */
 package io.github.kvverti.colormatic.mixin.color;
 
+import io.github.kvverti.colormatic.Colormatic;
+import io.github.kvverti.colormatic.colormap.BiomeColormap;
 import io.github.kvverti.colormatic.colormap.BiomeColormaps;
 
 import net.minecraft.block.BlockState;
@@ -24,9 +26,7 @@ import net.minecraft.client.color.world.BiomeColors;
 import net.minecraft.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
-import net.minecraft.world.biome.Biome;
 
-import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -38,14 +38,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(BiomeColors.class)
 public abstract class BiomeColorsMixin {
 
-    // @Dynamic("water color lambda method")
-    // @Inject(method = "method_4963", at = @At("HEAD"), cancellable = true)
-    // private static void onWaterColor(Biome biome, BlockPos pos, CallbackInfoReturnable<Integer> info) {
-    //     if(Colormatic.WATER_COLORS.hasCustomColormap()) {
-    //         info.setReturnValue(Colormatic.WATER_COLORS.getColormap().getColor(biome, pos));
-    //     }
-    // }
-
     /**
      * The FluidRenderer calls BiomeColors#getWaterColor directly, instead of
      * going through BlockColors. So, we must check for custom water color here.
@@ -55,6 +47,9 @@ public abstract class BiomeColorsMixin {
         BlockState state = world.getBlockState(pos);
         if(state.getFluidState().matches(FluidTags.WATER) && BiomeColormaps.isCustomColored(state)) {
             info.setReturnValue(BiomeColormaps.getBiomeColor(state, world, pos));
+        } else if(Colormatic.WATER_COLORS.hasCustomColormap()) {
+            BiomeColormap colormap = Colormatic.WATER_COLORS.getColormap();
+            info.setReturnValue(BiomeColormap.getBiomeColor(world, pos, colormap));
         }
     }
 }
