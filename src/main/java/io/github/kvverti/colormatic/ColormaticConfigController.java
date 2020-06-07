d2ae6e2c9a7250d98e79dd5cecccaa45b1e33132
Thalia Nero <chrisvn00@gmail.com>
2020-06-07 16:53:46 -0400

    Add ModMenu config support again.

--- src/main/java/io/github/kvverti/colormatic/ColormaticConfigController.java
+++ src/main/java/io/github/kvverti/colormatic/ColormaticConfigController.java
@@ -17,23 +17,21 @@
  */
 package io.github.kvverti.colormatic;
 
-import java.io.IOException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-
-import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
-import net.fabricmc.loader.api.FabricLoader;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.TranslatableText;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
+import net.fabricmc.loader.api.FabricLoader;
 
 /**
  * This class holds functions that control loading, opening, and persisting
@@ -47,7 +45,8 @@ public final class ColormaticConfigController {
 
     private static final ColormaticConfig defaults = new ColormaticConfig();
 
-    private ColormaticConfigController() {}
+    private ColormaticConfigController() {
+    }
 
     public static Screen getConfigScreen(ColormaticConfig config, Screen parent) {
         ConfigBuilder builder = ConfigBuilder.create()
--- src/main/java/io/github/kvverti/colormatic/ColormaticModMenu.java
+++ src/main/java/io/github/kvverti/colormatic/ColormaticModMenu.java
@@ -1,37 +1,34 @@
-///*
-// * Colormatic
-// * Copyright (C) 2019  Thalia Nero
-// *
-// * This program is free software: you can redistribute it and/or modify
-// * it under the terms of the GNU Lesser General Public License as published by
-// * the Free Software Foundation, either version 3 of the License, or
-// * (at your option) any later version.
-// *
-// * This program is distributed in the hope that it will be useful,
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// * GNU Lesser General Public License for more details.
-// *
-// * You should have received a copy of the GNU Lesser General Public License
-// * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-// */
-//package io.github.kvverti.colormatic;
-//
-//import io.github.prospector.modmenu.api.ModMenuApi;
-//
-//import java.util.function.Function;
-//
-//import net.minecraft.client.gui.screen.Screen;
-//
-//public class ColormaticModMenu implements ModMenuApi {
-//
-//    @Override
-//    public String getModId() {
-//        return Colormatic.MODID;
-//    }
-//
-//    @Override
-//    public Function<Screen, ? extends Screen> getConfigScreenFactory() {
-//        return parent -> ColormaticConfigController.getConfigScreen(Colormatic.config(), parent);
-//    }
-//}
+/*
+ * Colormatic
+ * Copyright (C) 2019-2020  Thalia Nero
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.github.kvverti.colormatic;
+
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
+
+public class ColormaticModMenu implements ModMenuApi {
+
+    @Override
+    public String getModId() {
+        return Colormatic.MODID;
+    }
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> ColormaticConfigController.getConfigScreen(Colormatic.config(), parent);
+    }
+}
