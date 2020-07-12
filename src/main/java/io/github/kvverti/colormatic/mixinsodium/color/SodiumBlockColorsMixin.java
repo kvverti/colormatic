2960b61f263fe34f6f371fda6111fd02f2184d68
Thalia Nero <chrisvn00@gmail.com>
2020-07-11 20:29:38 -0400

    Fix crash when installed with Sodium.

---
+++ src/main/java/io/github/kvverti/colormatic/mixinsodium/color/SodiumBlockColorsMixin.java
@@ -0,0 +1,33 @@
+package io.github.kvverti.colormatic.mixinsodium.color;
+
+import io.github.kvverti.colormatic.colormap.BiomeColormaps;
+import me.jellysquid.mods.sodium.client.world.biome.BlockColorsExtended;
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Intrinsic;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.client.color.block.BlockColorProvider;
+import net.minecraft.client.color.block.BlockColors;
+
+@Mixin(value = BlockColors.class, priority = 1001)
+@Implements(@Interface(iface = BlockColorsExtended.class, prefix = "i$", remap = Interface.Remap.NONE))
+public abstract class SodiumBlockColorsMixin implements BlockColorsExtended {
+
+    @Unique
+    private static final BlockColorProvider COLORMATIC_PROVIDER =
+        (state, world, pos, tintIndex) -> BiomeColormaps.getBiomeColor(state, world, pos);
+
+    /**
+     * Displace Sodium's implementation to first check Colormatic's custom block colors.
+     */
+    @Intrinsic(displace = true)
+    public BlockColorProvider i$getColorProvider(BlockState state) {
+        if(BiomeColormaps.isCustomColored(state)) {
+            return COLORMATIC_PROVIDER;
+        }
+        return this.getColorProvider(state);
+    }
+}
---
+++ src/main/java/io/github/kvverti/colormatic/mixinsodium/world/WorldSliceMixin.java
@@ -0,0 +1,43 @@
+package io.github.kvverti.colormatic.mixinsodium.world;
+
+import io.github.kvverti.colormatic.colormap.ColormaticBlockRenderView;
+import io.github.kvverti.colormatic.colormap.ColormaticResolver;
+import me.jellysquid.mods.sodium.client.world.WorldSlice;
+import me.jellysquid.mods.sodium.common.util.pool.ReusableObject;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
+
+@Mixin(WorldSlice.class)
+public abstract class WorldSliceMixin extends ReusableObject implements ColormaticBlockRenderView {
+
+    @Shadow
+    public native Biome getCachedBiome(int x, int z);
+
+    /**
+     * Copied from {@link io.github.kvverti.colormatic.mixin.world.ClientWorldMixin} for efficiency. The
+     * implementations must be kept in sync.
+     */
+    @Override
+    public int colormatic_getColor(BlockPos pos, ColormaticResolver resolver) {
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        int radius = MinecraftClient.getInstance().options.biomeBlendRadius;
+        Iterable<BlockPos> coll = BlockPos.iterate(
+            pos.getX() - radius, pos.getY(), pos.getZ() - radius,
+            pos.getX() + radius, pos.getY(), pos.getZ() + radius);
+        for(BlockPos curpos : coll) {
+            Biome biome = this.getCachedBiome(curpos.getX(), curpos.getZ());
+            int color = resolver.getColor(biome, curpos);
+            r += (color & 0xff0000) >> 16;
+            g += (color & 0x00ff00) >> 8;
+            b += (color & 0x0000ff);
+        }
+        int posCount = (radius * 2 + 1) * (radius * 2 + 1);
+        return ((r / posCount & 255) << 16) | ((g / posCount & 255) << 8) | (b / posCount & 255);
+    }
+}
