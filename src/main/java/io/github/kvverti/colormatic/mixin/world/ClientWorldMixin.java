afea43d4cad7dddff35e46ce7a11d982ad759425
Thalia Nero <chrisvn00@gmail.com>
2020-08-22 17:29:49 -0400

    Fix uncached biome color blending in vanilla.

--- src/main/java/io/github/kvverti/colormatic/mixin/world/ClientWorldMixin.java
+++ src/main/java/io/github/kvverti/colormatic/mixin/world/ClientWorldMixin.java
@@ -17,16 +17,23 @@
  */
 package io.github.kvverti.colormatic.mixin.world;
 
+import java.util.Map;
+
 import io.github.kvverti.colormatic.Colormatic;
 import io.github.kvverti.colormatic.colormap.BiomeColormap;
 import io.github.kvverti.colormatic.colormap.BiomeColormaps;
 import io.github.kvverti.colormatic.colormap.ColormaticBlockRenderView;
 import io.github.kvverti.colormatic.colormap.ColormaticResolver;
+import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.BiomeColorCache;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -66,8 +73,20 @@ public abstract class ClientWorldMixin extends World implements ColormaticBlockR
         return self.getSkyColor();
     }
 
+    /**
+     * Color cache map for custom biome colormaps.
+     */
+    @Unique
+    private final Map<ColormaticResolver, BiomeColorCache> customColorCache = new Reference2ObjectOpenHashMap<>();
+
     @Override
     public int colormatic_getColor(BlockPos pos, ColormaticResolver resolver) {
+        BiomeColorCache cache = customColorCache.computeIfAbsent(resolver, k -> new BiomeColorCache());
+        return cache.getBiomeColor(pos, () -> computeBiomeColor(pos, resolver));
+    }
+
+    @Unique
+    private int computeBiomeColor(BlockPos pos, ColormaticResolver resolver) {
         int r = 0;
         int g = 0;
         int b = 0;
@@ -84,5 +103,21 @@ public abstract class ClientWorldMixin extends World implements ColormaticBlockR
         }
         int posCount = (radius * 2 + 1) * (radius * 2 + 1);
         return ((r / posCount & 255) << 16) | ((g / posCount & 255) << 8) | (b / posCount & 255);
+    }
+
+    /**
+     * Reset custom colors for a chunk.
+     */
+    @Inject(method = "resetChunkColor", at = @At("RETURN"))
+    private void resetColormaticChunkColor(int x, int z, CallbackInfo info) {
+        customColorCache.forEach((resolver, cache) -> cache.reset(x, z));
+    }
+
+    /**
+     * Reset custom colors for the entire world.
+     */
+    @Inject(method = "reloadColor", at = @At("RETURN"))
+    private void reloadColormaticColor(CallbackInfo info) {
+        customColorCache.clear();
     }
 }
