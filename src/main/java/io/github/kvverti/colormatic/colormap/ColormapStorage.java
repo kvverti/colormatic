cb874015f91dd7dc1e9953dbae591415003c043e
Thalia Nero <chrisvn00@gmail.com>
2021-08-02 17:33:25 -0400

    Explicitly exempt Minecraft from source distribution requirement.

--- src/main/java/io/github/kvverti/colormatic/colormap/ColormapStorage.java
+++ src/main/java/io/github/kvverti/colormatic/colormap/ColormapStorage.java
@@ -7,6 +7,10 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
+ * As an additional permission, when conveying the Corresponding Source of an
+ * object code form of this work, you may exclude the Corresponding Source for
+ * "Minecraft" by Mojang Studios, AB.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- src/main/java/io/github/kvverti/colormatic/mixin/network/GameJoinS2CPacketMixin.java
+++ src/main/java/io/github/kvverti/colormatic/mixin/network/GameJoinS2CPacketMixin.java
@@ -1,11 +1,15 @@
 /*
  * Colormatic
- * Copyright (C) 2020  Thalia Nero
+ * Copyright (C) 2021  Thalia Nero
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
+ * As an additional permission, when conveying the Corresponding Source of an
+ * object code form of this work, you may exclude the Corresponding Source for
+ * "Minecraft" by Mojang Studios, AB.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
