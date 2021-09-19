9f8c64d1fab03adb01c1879324757511cbb10cfc
Thalia Nero <chrisvn00@gmail.com>
2021-09-19 16:25:15 -0500

    Use a mixin plugin to disable Sodium compatibility mixins when Sodium is not present.

---
+++ src/main/java/io/github/kvverti/colormatic/mixinsodium/SodiumMixinFilterPlugin.java
@@ -0,0 +1,49 @@
+package io.github.kvverti.colormatic.mixinsodium;
+
+import java.util.List;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import net.fabricmc.loader.api.FabricLoader;
+
+/**
+ * Apply Sodium mixins only when Sodium is present.
+ */
+public class SodiumMixinFilterPlugin implements IMixinConfigPlugin {
+
+    private final boolean shouldLoadMixins = FabricLoader.getInstance().isModLoaded("sodium");
+
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return this.shouldLoadMixins;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
